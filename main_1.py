99903a0604dba051f61d5a39a585c72ff70311db
teaca_daniele <teaca.daniele@iisgalvanimi.edu.it>
2022-10-13 10:57:13 +0000

    versione 1

---
+++ main_1.py
@@ -0,0 +1,9 @@
+x =  44       #dichiaro una variabile che è un numero intero 
+y = "Teaca"   #dichiaro una variabile che è una parola 
+z = "4.4"     #dichiaro una variabile che è un numero con la virgola 
+print(type(x))   #stampo variabile x, mi esce tipo variabile 
+print(type(y))   #stampo variabile y, mi esce tipo variabile 
+print(type(z))   #stampo variabile z, mi esce tipo variabile 
+print(x)       #stampo variabile x, e mi comparira il numero intero
+print(y)       #stampo variabile y, e mi comparira la parola 
+print(z)       #stampo variabile z,e mi comparira il numero decimale 
