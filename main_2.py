2cf3b64dc8d3bb01c76abbf1ce5a1b320fcad3a9
teaca_daniele <teaca.daniele@iisgalvanimi.edu.it>
2022-10-20 10:37:08 +0000

    versione 2

--- main_2.py
+++ main_2.py
@@ -1,3 +1,6 @@
 #creo una lista per stampare un insieme di elementi gi 
 thislist = ["bommo", "44", "1.7"]
 print(thislist)
+#con il comando len possiamo vissualizare il numero 
+thislist = ["bommo", "44", "1.6"]
+print(len(thislist))
