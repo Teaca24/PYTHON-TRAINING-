ef5957d1e09581d3c077272068740ead652ea244
teaca_daniele <teaca.daniele@iisgalvanimi.edu.it>
2022-10-20 10:40:50 +0000

    versione 3

--- main_2.py
+++ main_2.py
@@ -4,3 +4,6 @@ print(thislist)
 #con il comando len possiamo vissualizare il numero 
 thislist = ["bommo", "44", "1.6"]
 print(len(thislist))
+#con il comando type vedi che tipo di variabili hai nella lista 
+mylist = ["bommo", "44", "1.8"]
+print(type(mylist))
